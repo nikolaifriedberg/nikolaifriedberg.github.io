611a28249b1f7d841353761258f825aab23c969d
nikolaifriedberg <friedbergn@reed.edu>
2025-07-14 13:51:27 -0700

    Update notebook-data.js

--- notebook-data.js
+++ notebook-data.js
@@ -1,1 +1,19 @@
-
+window.NOTES = [
+  {
+    date: "July 14",
+    text: "I'm making a website for PJ right now. I'm having more fun than I expected."
+  },
+  {
+    date: "May 12",
+    text: "June lifts the soggy drop cloth covering the chair in the middle of the sidewalk. Turns around and looks up at the swaying oaks and listing willows. He sits down and listens to the world turn."
+  },
+  {
+    date: "December 25",
+    text: "It was the liquid. That's what finally did it. I'd been giving her gifts for a while, things I'd bought, things I'd found, and now things I'd made. She seemed to like it when I gave her gifts, though mostly because she liked me, I think. But she didn't ilke the liquid."
+  },
+  {
+    date: "September 11",
+    text: "BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM KERPOW *screams*"
+  }
+];
+      
